14ee3f1dbca11b0db5059f9556da4a8f640ccb84
May Bär <m.baer@iasonconsulting.com>
2025-10-21 09:57:53 +0200

    Add validation

--- src/Dtos/ListPollDto.cs
+++ src/Dtos/ListPollDto.cs
@@ -8,4 +8,9 @@ public class ListPostDto
 
 public class ListPollDto : CreatePollDto
 {
+    public new int MinValue { get; set; }
+    public new int MaxValue { get; set; }
+
+    public new int MinVotes { get; set; }
+    public new int MaxVotes { get; set; }
 }
