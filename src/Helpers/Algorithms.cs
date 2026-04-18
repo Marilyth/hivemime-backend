d2f987b64614cef9b68f4d1600584fd0acf1686e
May <baermay98@gmail.com>
2026-04-18 07:26:57 +0200

    Adjust formula

--- src/Helpers/Algorithms.cs
+++ src/Helpers/Algorithms.cs
@@ -3,9 +3,24 @@ using Microsoft.EntityFrameworkCore.Query;
 
 public static class Algorithms
 {
-    private static readonly double HotnessBase = Math.Log(100);
-    private const int HalflifeDays = 7;
+    /// <summary>
+    /// A value to boost new posts over old ones.
+    /// </summary>
+    private static readonly double BaseScore = 100;
+
+    /// <summary>
+    /// The amount of hours it takes for the hotness to halve.
+    /// </summary>
+    private const double HalflifeHours = 24;
+
+    /// <summary>
+    /// The weight of a vote in the hotness calculation.
+    /// </summary>
     private const double VoteWeight = 1;
+
+    /// <summary>
+    /// The weight of a comment in the hotness calculation.
+    /// </summary>
     private const double CommentWeight = 0.5;
 
     public static UpdateSettersBuilder<Post> UpdateHotness(this UpdateSettersBuilder<Post> builder, int commentDifference = 0, int voteDifference = 0)
@@ -22,9 +37,10 @@ public static class Algorithms
     }
     
     public static Expression<Func<Post, double>> GetHotnessUpdateExpression(int commentDifference = 0, int voteDifference = 0) =>
-        post => Math.Log(1 + (post.VoteCount + voteDifference) * VoteWeight +
-                         (post.CommentCount + commentDifference) * CommentWeight) + // Base score.
-                HotnessBase / Math.Pow(2, (DateTimeOffset.UtcNow - post.CreatedAt).TotalDays / HalflifeDays); // Time decayed bonus.
+        post => Math.Log(BaseScore +
+                         (post.VoteCount + voteDifference) * VoteWeight +
+                         (post.CommentCount + commentDifference) * CommentWeight) /
+                Math.Pow(2, (DateTimeOffset.UtcNow - post.CreatedAt).TotalHours / HalflifeHours);
 
     public static Func<Post, double> HotnessFunction = GetHotnessUpdateExpression().Compile();
 }
