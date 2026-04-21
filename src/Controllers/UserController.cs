ec8be92393f2de57e82a14c33512fb48a5305e8c
May Bär <m.baer@iasonconsulting.com>
2026-04-21 11:00:23 +0200

    Fix manual JWT decoding not working

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -1,15 +1,23 @@
 using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UserController(UserService userService, HiveMimeContext context) : ControllerBase
+public class UserController(UserService userService, HiveMimeContext context, IOptionsMonitor<JwtBearerOptions> optionsMonitor) : ControllerBase
 {
     [HttpGet("merge")]
     public async Task MergeAccounts(string previousJwt)
     {
+        var jwtOptions = optionsMonitor.Get(JwtBearerDefaults.AuthenticationScheme);
+
+        jwtOptions.TokenValidationParameters.ConfigurationManager =
+            (BaseConfigurationManager)jwtOptions.ConfigurationManager;
+            
         JwtSecurityTokenHandler jwtHandler = new();
-        var result = jwtHandler.ValidateToken(previousJwt, Program.Parameters, out _);
+        var result = jwtHandler.ValidateToken(previousJwt, jwtOptions.TokenValidationParameters, out _);
 
         // Merges must be on an anonymous account through the API.
         if (result.FindFirst("provider_id")!.Value != "anonymous")
