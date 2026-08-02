4253f5e795bc972b9095c6e85e98c46ac5addf3a
May <baermay98@gmail.com>
2026-08-02 22:13:19 +0200

    Add new operators

--- src/Models/VoteQuery/ValueOperator.cs
+++ src/Models/VoteQuery/ValueOperator.cs
@@ -11,5 +11,14 @@ public enum ValueOperator
     [Description("<")]
     Less,
     [Description("<=")]
-    LessEquals
+    LessEquals,
+
+    [Description("in")]
+    Inside,
+    [Description("out")]
+    Outside,
+    [Description("xin")]
+    ExclusiveInside,
+    [Description("xout")]
+    ExclusiveOutside
 }
